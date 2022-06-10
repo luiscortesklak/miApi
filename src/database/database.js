b10de009906cc05bb7717c6a38e5cf842d811da8
luiscortesklak <88160789+luiscortesklak@users.noreply.github.com>
2022-06-10 14:58:53 -0300

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import config from '../config';
 
 (async()=>{
-const db= await mongoose.connect(mongodb://localhost/basesTecnicas);   
+const db= await mongoose.connect('mongodb://localhost/basesTecnicas');   
  console.log(db.connection.name);
 })();
  
