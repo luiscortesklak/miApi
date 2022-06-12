b21b525ca9fd33160c2dd9ae103d526cba3b837a
luiscortesklak <luis.cortes@davinci.edu.ar>
2022-06-12 17:32:06 -0300

    Update coordenadas.controller.js

--- src/controllers/coordenadas.controller.js
+++ src/controllers/coordenadas.controller.js
@@ -14,3 +14,28 @@ export const buscarPorIdCoordenadas = async (req, res) => {
     }
 
 };
+export const nuevasCoordenadas = async (req, res) => {
+
+
+    try {
+        const coors = new Coordenadas({
+            id_coordenadas: req.body.id_coordenadas,
+            latitud_uno: req.body.latitud_uno,
+            latitud_dos: req.body.latitud_dos,
+            latitud_tres: req.body.latitud_tres,
+            latitud_cuatro: req.body.latitud_cuatro,
+            longitud_uno: req.body.longitud_uno,
+            longitud_dos: req.body.longitud_dos,
+            longitud_tres: req.body.longitud_tres,
+            longitud_cuatro: req.body.longitud_cuatro,
+        });
+        await coors.save();
+        console.log(coors);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+
+};
