8d37210cea514be8a9a1a4380b310edee2dd10f4
luiscortesklak <luis.cortes@davinci.edu.ar>
2022-06-12 17:35:06 -0300

    Update tareas.js

--- src/routes/tareas.js
+++ src/routes/tareas.js
@@ -3,6 +3,7 @@ import * as empleadoCtrl from "../controllers/empleado.controller";
 import * as baseCtrl from "../controllers/base.controller";
 import * as coorCtrl from "../controllers/coordenadas.controller";
 import * as asisCtrl from "../controllers/asistencia.controller";
+import * as limCtrl from "../controllers/limites.controller";
 const router =Router();
 //peticiones de empleado
 
@@ -20,6 +21,7 @@ router.get ('/base/:id_base',baseCtrl.buscarPorBase);
 
 router.get ('/coordenadas/:id_coordenadas',coorCtrl.buscarPorIdCoordenadas);
 
+router.post ('/coordenadas',coorCtrl.nuevasCoordenadas);
 // peticiones de asistencia
 
 router.post ('/asis',asisCtrl.nuevaAsistencia);
@@ -27,5 +29,14 @@ router.post ('/asis',asisCtrl.nuevaAsistencia);
 router.get ('/asis',asisCtrl.totalDeRegistro);
 
 router.get ('/asis/:legajo',asisCtrl.buscarPorlegajo);
+
+// peticiones limites
+
+router.get ('/limites/:id_base',limCtrl.buscarPorlimites);
+
+router.post ('/limites',limCtrl.nuevosLimites);
+
+router.get ('/limites',limCtrl.limitesTotal);
+
 
 export default router;
